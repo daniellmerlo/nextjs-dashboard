dc9004a1d2d17fa058b5c9049189274d4ee23abc
Daniel Merlo <daniellmerlo@gmail.com>
2025-04-16 12:52:43 -0300

    Updates ate o capitulo 14

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -105,12 +105,15 @@ async function seedRevenue() {
 
 export async function GET() {
   try {
-    const result = await sql.begin((sql) => [
+    const result = await sql.begin(() => [
       seedUsers(),
       seedCustomers(),
       seedInvoices(),
       seedRevenue(),
     ]);
+
+    console.log(result);
+    console.log(sql);
 
     return Response.json({ message: 'Database seeded successfully' });
   } catch (error) {
